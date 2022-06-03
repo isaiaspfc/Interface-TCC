436f5e536b29df017fc315b7c84cd03fcba043ea
isaiaspfc <isaiaspfc@hotmail.com>
2022-06-03 16:32:14 -0300

    Update serv.js

---
+++ serv.js
@@ -0,0 +1,16 @@
+const compression = require('compression')
+const express = require('express');
+
+var app = express();
+app.use(compression({ filter: shouldCompress }))
+
+function shouldCompress (req, res) {
+    if (req.headers['x-no-compression']) {
+        return false
+    }
+
+    return compression.filter(req, res)
+}
+
+app.listen(5000);
+app.use("/", express.static( __dirname + "/"));
